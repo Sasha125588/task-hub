d261f19641625563c9fcec5583413ce1095d5ed7
sasha125588 <sashavolskiy11@gmail.com>
2025-07-02 23:20:21 +0300

    Added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import type { PropsWithChildren } from "react"
@@ -44,6 +45,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
 				>
 					<ScrollProgress />
 					{children}
+					<Analytics />
 				</ThemeProvider>
 			</body>
 		</html>
