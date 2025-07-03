3c3b08b0a76904daad710a5761c4d8c5f42a8b4b
sasha125588 <sashavolskiy11@gmail.com>
2025-07-04 02:36:53 +0300

    added suspens to dashboard/page

--- src/app/(sidebar)/dashboard/page.tsx
+++ src/app/(sidebar)/dashboard/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from "react"
+
 import { DashboardContent } from "@/components/sidebar/dashboard/DashboardContent"
 
 export default function DashboardPage() {
-	return <DashboardContent />
+	return (
+		<Suspense fallback={<>Loading dashboard page</>}>
+			<DashboardContent />
+		</Suspense>
+	)
 }
