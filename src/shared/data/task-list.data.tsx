8f431ff9b693463412c362ebc7f93049c03baf35
sasha125588 <sashavolskiy11@gmail.com>
2025-07-11 00:37:11 +0300

    integrate Amazon Cognito authentication

--- src/shared/data/task-list.data.tsx
+++
@@ -1,8 +0,0 @@
-import type { ITabs } from "@/components/sidebar/dashboard/last-tasks/TaskList"
-
-export const TABS: ITabs[] = [
-	{ title: "All", value: "all" },
-	{ title: "Completed", value: "completed" },
-	{ title: "In Progress", value: "in-progress" },
-	{ title: "Not started", value: "not-started" }
-] as const
