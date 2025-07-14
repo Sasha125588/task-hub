aa74404a037d59102ad162bc51838aecaa3b2056
sasha125588 <sashavolskiy11@gmail.com>
2025-07-14 20:15:26 +0300

    fix imports

--- client/src/shared/data/dashboard-statistics.data.tsx
+++ client/src/shared/data/dashboard-statistics.data.tsx
@@ -1,25 +1,25 @@
-import type { ICardItem } from "@/components/pages/dashboard/statistics/types";
+import type { StatisticsCard } from '@/types/statistics.card.types'
 
-export const STATISTICS_CARDS: ICardItem[] = [
-  {
-    title: "92",
-    desc: "Active Projects",
-    imgSrc: "/images/active-projects.svg",
-    colorClass: "bg-[#d6bcfa]",
-    darkColorClass: "dark:bg-[#6366f1]",
-  },
-  {
-    title: "35",
-    desc: "On Going Project",
-    imgSrc: "/images/ongoing-projects.svg",
-    colorClass: "bg-[#faf089]",
-    darkColorClass: "dark:bg-[#ac35d4]",
-  },
-  {
-    title: "19h 3m",
-    desc: "Working Hours",
-    imgSrc: "/images/working-hours.svg",
-    colorClass: "bg-[#fbb6ce]",
-    darkColorClass: "dark:bg-[#de5d8a]",
-  },
-];
+export const STATISTICS_CARDS: StatisticsCard[] = [
+	{
+		title: '92',
+		desc: 'Active Projects',
+		imgSrc: '/images/active-projects.svg',
+		colorClass: 'bg-[#d6bcfa]',
+		darkColorClass: 'dark:bg-[#6366f1]'
+	},
+	{
+		title: '35',
+		desc: 'On Going Project',
+		imgSrc: '/images/ongoing-projects.svg',
+		colorClass: 'bg-[#faf089]',
+		darkColorClass: 'dark:bg-[#ac35d4]'
+	},
+	{
+		title: '19h 3m',
+		desc: 'Working Hours',
+		imgSrc: '/images/working-hours.svg',
+		colorClass: 'bg-[#fbb6ce]',
+		darkColorClass: 'dark:bg-[#de5d8a]'
+	}
+]
