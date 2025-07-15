a0162e6ffc3227bed6cebdd88e185b2b8a6a0f43
sasha125588 <sashavolskiy11@gmail.com>
2025-07-15 21:49:00 +0300

    fix: update routes configuration

--- client/src/configs/pages.config.tsx
+++ client/src/configs/pages.config.tsx
@@ -3,12 +3,12 @@ export class PAGES_CONFIG {
 
 	static BASE_URL = '/'
 	static DASHBOARD_URL = this.BASE_URL + 'dashboard'
-	static MESSAGE_URL = this.BASE_URL + '/message'
-	static INSIGHT_URL = this.BASE_URL + '/insight'
-	static TEAM_URL = this.BASE_URL + '/team'
-	static SCHEDULE_URL = this.BASE_URL + '/schedule'
-	static REPORT_URL = this.BASE_URL + '/report'
-	static SETTINGS_URL = this.BASE_URL + '/settings'
+	static MESSAGE_URL = this.BASE_URL + 'message'
+	static INSIGHT_URL = this.BASE_URL + 'insight'
+	static TEAM_URL = this.BASE_URL + 'team'
+	static SCHEDULE_URL = this.BASE_URL + 'schedule'
+	static REPORT_URL = this.BASE_URL + 'report'
+	static SETTINGS_URL = this.BASE_URL + 'settings'
 
 	static EDIT_TASK_URL = (id: string) => this.DASHBOARD_URL + `/task/${id}/edit`
 }
